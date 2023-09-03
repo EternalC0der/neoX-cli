cc6681e418a063b2addbb2aadb0bf14830969a8a
EternalC0der <s.eternalcoder@gmail.com>
2023-09-04 01:14:27 +0330

    Minor improvements

--- src/cli.ts
+++ src/cli.ts
@@ -4,11 +4,13 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 yargs(hideBin(process.argv))
-    .scriptName('cross-types')
+    .scriptName('ct')
     .usage('$0 <cmd> [args]')
     // Use the commands directory to scaffold.
     .commandDir('commands')
     // Enable strict mode.
     .strict()
+    // Show help if no command is provided.
+    .demandCommand()
     // Useful aliases.
     .alias({ h: 'help', p: 'pull' }).argv
